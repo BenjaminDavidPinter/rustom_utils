aac370493a570ae8aee3c1daf44ee6d0b3f7e507
Benjamin Pinter <72715672+BenjaminDavidPinter@users.noreply.github.com>
2023-08-29 22:59:27 -0400

    Accumulated bytes per file

--- src/cache_inspect/mod.rs
+++ src/cache_inspect/mod.rs
@@ -1,5 +1,9 @@
 use std::fs;
 use std::path::Path;
+use std::io;
+use std::os::macos::fs::MetadataExt;
+
+use super::shared::ProgramArgs;
 
 const CACHE_PATH: &str = "/Users/benjaminpinter/Library/Caches";
 
@@ -17,7 +21,7 @@ const CACHE_PATH: &str = "/Users/benjaminpinter/Library/Caches";
         ...well it is the recursive function
 
 */
-pub fn scan_cache() {
+pub fn scan_cache(args : Vec<ProgramArgs>) {
     let cache_dirs_result = fs::read_dir(CACHE_PATH);
     match cache_dirs_result {
         Ok(cache_dirs) => {
@@ -32,7 +36,7 @@ pub fn scan_cache() {
                 //  function
                 match dir {
                     Ok(dir_info) => {
-                        println!("{:?}", dir_info);
+                        print!("{:?} - ", dir_info.file_name());
 
                         /*
                         It is finally at this point that we have reached the folders within
@@ -44,28 +48,23 @@ pub fn scan_cache() {
 
 
                         if dir_info.path().as_path().is_dir() {
-                            dive_folder(&dir_info.path().as_path(), 1);
+                            let total_file_size = dive_folder(&dir_info.path().as_path(), 1);
+                            println!("{} bytes", total_file_size);
                         }
-                        else {
-                            //This isolates files which are top-level non-directory files.
-                            // This inludes raw
-                            println!("F:{:?}", dir_info);
-                        }
-
-
                     },
-                    _ => println!("An error has occured")
+                    _ => print!("Error")
                 }
             }
         },
-        _ => println!("An error occured")
+        _ => print!("Error")
     };
 }
 
 //So that means, in here we are going to do nearly the same thing, but
 //  after printing out the directory tree, we call this again until we have no folders
 //  left to traverse.
-fn dive_folder(path: &Path, depth: i32) {
+fn dive_folder(path: &Path, depth: i32) -> u64 {
+    let mut total_file_size: u64 = 0;
     let prefix_string = generate_depth_tabs(depth);
     let cache_dirs_result = fs::read_dir(path.as_os_str());
     match cache_dirs_result {
@@ -73,27 +72,45 @@ fn dive_folder(path: &Path, depth: i32) {
             for dir in cache_dirs {
                 match dir {
                     Ok(dir_info) => {
-                        println!("{}{:?}", prefix_string, dir_info);
                         if dir_info.path().as_path().is_dir() {
-                            dive_folder(dir_info.path().as_path(), depth+1);
+                            total_file_size += dive_folder(dir_info.path().as_path(), depth+1);
                         }
                         else {
-                            println!("{}F:{:?}", prefix_string,dir_info);
+                            let metadata = fs::metadata(dir_info.path().as_path()).unwrap();
+                            total_file_size += metadata.st_size();
                         }
                     },
-                    _ => println!("An error has occured")
+                    _ => total_file_size += 0
                 }
             }
         },
-        _ => println!("An error occured")
+        _ => total_file_size += 0
     };
+
+    total_file_size
 }
 
-fn generate_depth_tabs(total: i32) -> String{
-    //Move this into its own function, it's messy in here
+fn generate_depth_tabs(total: i32) -> String {
     let mut depth_string = String::new();
-    for i in 0..total {
+    for _ in 0..total {
         depth_string += "\t";
     }
     return depth_string;
+}
+
+#[cfg(test)]
+pub mod cache_inspect_tests {
+    use super::*;
+
+    #[test]
+    fn tab_depth_1() {
+        let result = generate_depth_tabs(1);
+        assert_eq!(result, "\t");
+    }
+
+    #[test]
+    fn tab_depth_5() {
+        let result = generate_depth_tabs(5);
+        assert_eq!(result, "\t\t\t\t\t");
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,12 @@ use std::env;
 pub mod shared;
 use shared::*;
 pub mod cache_inspect;
-use cache_inspect::*;
 
 fn main() {
     let (program_select, program_args) = shared::handle_args(env::args().collect());
 
     match program_select {
-        ProgramSelect::CacheInspect => cache_inspect::scan_cache(),
+        ProgramSelect::CacheInspect => cache_inspect::scan_cache(program_args),
         _ => panic!("Unsupported program")
     };
 }
